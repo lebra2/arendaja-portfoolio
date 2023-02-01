aa5c6b6e0472420171aaa4889c82492c0fde3ed7
lebra2 <92708848+lebra2@users.noreply.github.com>
2023-02-01 13:48:09 +0200

    adg

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -3,9 +3,9 @@
     <div class="h-full 2xl:mx-48 2xl:px-48 lg:px-24 lg:mx-24">
         <div class="text-left h-full mx-4">
             <div class="mt-8 rounded text-xl flex flex-col gap-2">
-                <h2 class="text-8xl font-['South'] text-white">hei ! minu nimi on</h2>
-                <h1 class="text-9xl text-white mt-2 font-['South']">MARKUS</h1>
-                <span class="font-['Jetbrains'] text-4xl text-white">Mulle meeldib <b class="text-black">kood</b>, <b class="text-black">Disain</b> ja <b class="text-black"> muusika</b></span>
+                <h2 class="text-6xl lg:text-8xl font-['South'] text-white">hei ! minu nimi on</h2>
+                <h1 class="text-8xl lg:text-9xl text-white mt-2 font-['South'] text-shadow-lg">MARKUS</h1>
+                <span class="font-['Jetbrains'] text-4xl text-white">Mulle meeldib <b class="text-yellow-300 text-shadow-lg">kood</b>, <b class="text-yellow-300 text-shadow-lg">disain</b> ja <b class="text-yellow-300 text-shadow-lg"> muusika</b></span>
                 <p class="text-white lg:text-2xl font-semibold mt-4 font-['Jetbrains']">Olen 17-aastane noorem tarkvaraarendaja ja õpin <b class="text-lime-300">Kuressaare Ametikoolis.</b> <br> Peamiselt olen tegelenud Vue JS / Tailwindiga. <br class=""> Olen õppinud veel Pythonit, Javascripti, MySQLI, Linuxi Terminali, PHP-d.
                 <br> Olen iseloomult sõbralik ja õpihimuline. Meeldib meeskonda juhtida ja erinevaid probleeme lahendada. <br>
                     Vabal ajal tegelen muusika loomisega ehk räppimise ja instrumentaalide tegemisega. Kasutan enda muusikaga tegemiseks FL Studiot ja mu loovusega saab tutvuda alloleval lingil.</p>
