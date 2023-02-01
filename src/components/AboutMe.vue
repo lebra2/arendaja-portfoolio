bffb4fd83b257d4082dcac8068a9d9f20b539d75
lebra2 <92708848+lebra2@users.noreply.github.com>
2023-02-01 13:38:53 +0200

    changes

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,12 +1,12 @@
 <template> 
 
-    <div class="h-full lg:mx-48">
+    <div class="h-full 2xl:mx-48 2xl:px-48 lg:px-24 lg:mx-24">
         <div class="text-left h-full mx-4">
-            <div class="mt-8 rounded text-xl flex flex-col gap-2 ">
-                <h2 class="text-4xl font-['South'] text-white">hei.. minu nimi on</h2>
-                <h1 class="text-6xl md:text-center text-white mt-2 font-['South']">MARKUS</h1>
-                <h3 class="font-['Start'] text-white">Mulle meeldib kood,</h3> <h3 class="text-white  text-4xl font-['Bangers']">disain.. ja</h3> <h3 class="text-white">muusika</h3>  <h3 class="font-['Start'] text-white">Need kolm asja on omavahel seotud</h3>
-                <p class="text-white lg:text-2xl font-semibold mt-4">Olen 17-aastane noorem tarkvaraarendaja ja õpin Kuressaare Ametikoolis. <br> Peamiselt olen tegelenud Vue JS / Tailwindiga. <br class=""> Olen õppinud veel Pythonit, Javascripti, MySQLI, Linuxi Terminali, PHP-d.
+            <div class="mt-8 rounded text-xl flex flex-col gap-2">
+                <h2 class="text-8xl font-['South'] text-white">hei ! minu nimi on</h2>
+                <h1 class="text-9xl text-white mt-2 font-['South']">MARKUS</h1>
+                <span class="font-['Jetbrains'] text-4xl text-white">Mulle meeldib <b class="text-black">kood</b>, <b class="text-black">Disain</b> ja <b class="text-black"> muusika</b></span>
+                <p class="text-white lg:text-2xl font-semibold mt-4 font-['Jetbrains']">Olen 17-aastane noorem tarkvaraarendaja ja õpin <b class="text-lime-300">Kuressaare Ametikoolis.</b> <br> Peamiselt olen tegelenud Vue JS / Tailwindiga. <br class=""> Olen õppinud veel Pythonit, Javascripti, MySQLI, Linuxi Terminali, PHP-d.
                 <br> Olen iseloomult sõbralik ja õpihimuline. Meeldib meeskonda juhtida ja erinevaid probleeme lahendada. <br>
                     Vabal ajal tegelen muusika loomisega ehk räppimise ja instrumentaalide tegemisega. Kasutan enda muusikaga tegemiseks FL Studiot ja mu loovusega saab tutvuda alloleval lingil.</p>
                     <a class="text-lime-300 text-2xl font-semibold italic" href="https://www.lebra.ee/">www.lebra.ee</a>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,12 +1,27 @@
 <template>
-    <div class="mt-4">
-        <h1 class="text-white text-center md:text-left text-4xl">Projektid</h1>
-        <div class="mt-12 flex gap-12 text-center text-white font-semibold text-2xl ">
-            <div class="mt-4 bg-black w-72 h-72 items-center justify-center flex">
-                <h1 class="">Laravel - <br> veebipood</h1>
+    <div class="p-4">
+        <h1 class="lg:px-24 lg:mx-24 2xl:mx-48 2xl:px-48 text-white text-3xl mt-4 font-['Jetbrains']">Projektid:</h1>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-2 lg:px-24 lg:mx-24 2xl:mx-48 2xl:px-48">
+    
+            <div class="rounded-md w-full h-72 bg-black flex justify-center items-center">
+                <h1 class="text-white text-center">Projekt 1</h1>
             </div>
-
-        </div>
-
+            <div class="rounded-md w-full h-72 bg-black flex justify-center items-center">
+                <h1 class="text-white text-center">Projekt 1</h1>
+            </div>
+            <div class="rounded-md w-full h-72 bg-black flex justify-center items-center">
+                <h1 class="text-white text-center">Projekt 1</h1>
+            </div>
+            <div class="rounded-md w-full h-72 bg-black flex justify-center items-center">
+                <h1 class="text-white text-center">Projekt 1</h1>
+            </div>
+            <div class="rounded-md w-full h-72 bg-black flex justify-center items-center">
+                <h1 class="text-white text-center">Projekt 1</h1>
+            </div>
+            <div class="rounded-md w-full h-72 bg-black flex justify-center items-center">
+                <h1 class="text-white text-center">Projekt 1</h1>
+            </div>
+        </div> 
     </div>
+
 </template>
